07a34c68f2857e082ed77e30bd1178e872a4491c
aliyar <toktogulovaliyar24@gmail.com>
2026-08-05 02:15:44 +0500

    connect frontend to backend

--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: BuildEnv) => {
 
     const mode = env?.mode || 'development';
     const PORT = env?.port || 3000;
-    const apiUrl = env?.apiUrl || 'http://localhost:8000';
+    const apiUrl = env?.apiUrl || 'https://blog-production-project-1.onrender.com/';
 
     const isDev = mode === 'development';
 
