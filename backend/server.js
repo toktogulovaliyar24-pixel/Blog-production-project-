67f031ca13d983c655468a44db9b003984d27c81
aliyar <toktogulovaliyar24@gmail.com>
2026-08-05 02:39:41 +0500

    add login endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,29 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+
+server.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = router.db
+        .get('users')
+        .find({ username, password })
+        .value();
+
+    if (user) {
+        return res.json(user);
+    }
+
+    return res.status(401).json({
+        message: 'User not found',
+    });
+});
+
+
 server.use(router);
+
 
 server.listen(8000, '0.0.0.0', () => {
     console.log('Server started on 8000');
